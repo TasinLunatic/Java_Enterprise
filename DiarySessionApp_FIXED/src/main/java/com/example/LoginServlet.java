3fca22ea04927cfd72c4c9e6fdd0bdbd40ae96e9
TasinLunatic <ahmedtasin207@gmail.com>
2026-05-07 22:48:46 +0800

    reformation loginservlet as like as html

--- DiarySessionApp_FIXED/src/main/java/com/example/LoginServlet.java
+++ DiarySessionApp_FIXED/src/main/java/com/example/LoginServlet.java
@@ -1,12 +1,8 @@
-
 package com.example;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
@@ -21,21 +17,27 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        if ("admin".equals(username) && "123456".equals(password)) {
+        // Simple login validation
+        if ("admin".equals(username)
+                && "123456".equals(password)) {
 
+            // Create session
             HttpSession session = request.getSession();
+
+            // Save username
             session.setAttribute("username", username);
 
+            // Redirect after login
             response.sendRedirect("diary");
 
         } else {
 
-            response.setContentType("text/html;charset=UTF-8");
+            response.setContentType("text/html");
 
-            response.getWriter().println(
-                    "<h2>Login Failed!</h2>" +
-                    "<a href='login.html'>Back</a>"
-            );
+            response.getWriter().println("""
+                    <h2>Login Failed!</h2>
+                    <a href='login.html'>Try Again</a>
+                    """);
         }
     }
 }
